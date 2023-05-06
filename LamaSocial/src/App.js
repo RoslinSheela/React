7977fdc5015f8a2e43dcbff921130162593bd6de
Roslin_sheela <130845680+RoslinSheela@users.noreply.github.com>
2023-05-06 21:20:13 +0530

    Add files via upload

---
+++ LamaSocial/src/App.js
@@ -0,0 +1,19 @@
+
+import Home from './pages/home/Home';
+import Profile from './pages/Profile/Profile';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+
+function App() {
+  return (
+    // <div>
+    //   {/* Calling Home Page */}
+    //     {/* <Home/>  */}
+        
+    // </div>
+    <Home/>
+    
+  );
+}
+
+export default App;
